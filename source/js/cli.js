8b6978ed6396eef11346e7fb75d10eb6da9a7511
LepronlineR <44305771+LepronlineR@users.noreply.github.com>
2024-08-12 22:38:00 -0400

    add echo

--- source/js/cli.js
+++ source/js/cli.js
@@ -49,6 +49,9 @@ document.addEventListener('DOMContentLoaded', () => {
         ls          List the directory.
         cd          Change the directory.
         clear       Clears the output.
+        echo        Echo.
+
+    Here are some example commands you can enter: [z help] [cd portfolio]
     </pre>`;
 
     async function initCLI() {
@@ -176,6 +179,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 break;
             case 'cd':
                 response = handleCDCommand(args);
+                break;
+            case 'echo':
+                response = command.substring(5);
                 break;
             case 'clear':
                 response = '';
@@ -360,6 +366,9 @@ document.addEventListener('DOMContentLoaded', () => {
             }
             return '';
         }
+
+        // separate the directory to a list of nodes instead of manually checking (use the same system for LS)
+        // TODO 
 
         if (args[1].startsWith('./')) { // case that inputs start with ./ for the directory location
             args[1] = args[1].substring(2);
